a8aaee07cd7a0b62dcb4d613a0abe7b6dd8d88e1
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-10-28 03:29:18 +0100

    saturday

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -25,10 +25,16 @@ const createNewEmployees = (req,res) =>{
 }
 
 const updateEmployees = (req,res) =>{
-    res.json({
-        "firstname":req.body.firstname,
-        "lastname": req.body.lastname
-    });
+   const employee = data.employees.find(map => emp.id == parseInt(req.body.id));
+   if (!employee) {
+    return res.status(408).json({"message":`employee ID ${req.body.id} not found`})
+   }
+   if(req.body.firstname) employee.firstname = req.body.firstname;
+   if(req.body.lastname) employee.lastname = req.body.lastname;
+   const filteredArray = data.employees.filter(map => emp.id !==parseInt(req.body.id));
+   const unsortedArray = [...filteredArray,employee];
+   data.setEmployee(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 :0));
+   res.json(data.employees);
 }
 
 const deleteEmployees = (req,res) =>{
