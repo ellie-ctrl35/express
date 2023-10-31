2d224df107a90d59c0d7e8a9568f1455bdbd6cea
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-10-31 11:52:55 +0000

    Tuesday

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,7 +1,30 @@
-const userDB = {
+const usersDB = {
     users:require('../data/users.json'),
     setUsers:function (data) {
         this.users=data}
 }
 const fsPromises = require('fs').promises;
 const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async(req,res) =>{
+    const {user, pwd}=req.body;
+    if(!user||!pwd) return res.status(400).json({'message':'Username and password are required.'})
+    
+    const duplicate = userDB.users.find(person.username == user);
+    if (duplicate) return res.sendStatus(409);
+    try {
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        const newUser = {"username":user, "password":hashedPwd};
+        usersDB.setUsers([...usersDB.users,newUser]);
+        await fsPromises.writeFile(path.join(__dirname,'..','data','users.json'),
+         JSON.stringify(usersDB.users)
+        );
+        console.log(usersDB.users);
+        res.status(201).json({'success':`New User ${user} created`});
+    } catch (error) {
+        res.send(500).json({'message':error.message});
+    }
+}
+
+module.exports = {handleNewUser};
