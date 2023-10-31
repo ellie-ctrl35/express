bc3e2ac0edb11503889360b6f86fb81c0b74b03c
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-10-31 11:33:04 +0000

    Tuesday

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,4 +3,5 @@ const userDB = {
     setUsers:function (data) {
         this.users=data}
 }
-const fsPromises = require('fs')
+const fsPromises = require('fs').promises;
+const path = require('path');
