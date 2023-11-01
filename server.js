36d2ef6827415992b9bd81316176539b9fa4cd25
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-11-01 17:21:52 +0000

    wednesday

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use('/',require('./routes/root'));
 
 app.use('/register',require('./routes/register'));
 
+app.use('/login',require('./routes/login'));
+
 app.use('/employees',require('./routes/api/employees'));
 
 app.all('*', (req,res) => {
