b9da0de583d1b255b860f58df9fc4fd9a1e69c17
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-11-02 07:05:15 +0000

    Thursday

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,11 @@ const usersDB = {
     setUsers:function (data) { this.users=data}
 }
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 
 const handleLogin = async (req,res) => {
     const {user, pwd}=req.body;
@@ -12,6 +17,23 @@ const handleLogin = async (req,res) => {
     if (!foundUser) return res.sendStatus(401);
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
+        const accessToken = jwt.sign(
+            {"username":foundUser.username},
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn:'30s'}
+        );
+        const refreshToken = jwt.sign(
+            {"username":foundUser.username},
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn:'1d '}
+        );
+        const otherUsers = usersDB.users.filter(person => person.username !==foundUser.username)
+        const currentUser = {...foundUser,refreshToken};
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname,'..','data','users.json'),
+            JSON.stringify(usersDB.users)
+        );
         res.json({'success' : `User ${user} is logged in`});
         console.log('Logged in');   
     }
