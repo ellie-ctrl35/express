c817c82fdaee209947a6249a061b54eb6deab154
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-11-02 07:42:48 +0000

    Thursday

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,12 +20,12 @@ const handleLogin = async (req,res) => {
         const accessToken = jwt.sign(
             {"username":foundUser.username},
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn:'30s'}
+            { expiresIn:'15m'}
         );
         const refreshToken = jwt.sign(
             {"username":foundUser.username},
             process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn:'1d '}
+            { expiresIn:'1d'}
         );
         const otherUsers = usersDB.users.filter(person => person.username !==foundUser.username)
         const currentUser = {...foundUser,refreshToken};
