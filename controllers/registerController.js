5dcc3513ea5bdd91d53ed55aa7098a4290d8d049
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-11-02 18:03:31 +0000

    Thursday

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,30 +1,34 @@
 const usersDB = {
-    users:require('../data/users.json'),
-    setUsers:function (data) {
-        this.users=data}
-}
-const fsPromises = require('fs').promises;
+    users: require('../data/users.json'),
+    setUsers: function(data) {
+        this.users = data;
+    }
+};
+
+const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-const handleNewUser = async(req,res) =>{
-    const {user, pwd}=req.body;
-    if(!user||!pwd) return res.status(400).json({'message':'Username and password are required.'})
-    
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
     const duplicate = usersDB.users.find(person => person.username === user);
     if (duplicate) return res.sendStatus(409);
+
     try {
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        const newUser = {"username":user, "password":hashedPwd};
-        usersDB.setUsers([...usersDB.users,newUser]);
-        await fsPromises.writeFile(path.join(__dirname,'..','data','users.json'),
-         JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
-        res.status(201).json({'success':`New User ${user} created`});
-    } catch (error) {
-        res.send(500).json({'message':error.message});
-    }
-}
+        const newUser = { "username": user, "password": hashedPwd };
+        usersDB.setUsers([...usersDB.users, newUser]);
 
-module.exports = {handleNewUser};
+        // Write data to the file synchronously
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json'), JSON.stringify(usersDB.users));
+
+        console.log(usersDB.users);
+        res.status(201).json({ 'success': `New User ${user} created` });
+    } catch (error) {
+        res.status(500).json({ 'message': error.message });
+    }
+};
+
+module.exports = { handleNewUser };
